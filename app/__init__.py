96b19a10ed67fbc32f8fa1888109da8a163f5019
alexfrancow <alejandrofrancovazquez@gmail.com>
2018-05-25 19:32:03 +0200

    Added scan page

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,11 @@ Licence: GPLv3
 
 from flask import Flask
 
-
 ################
 #### config ####
 ################
 
 app = Flask(__name__, instance_relative_config=True)
-
 
 ####################
 #### blueprints ####
@@ -24,7 +22,10 @@ app = Flask(__name__, instance_relative_config=True)
 
 from app.views import anomalies_blueprint
 from app.mod_dynamic.views import dynamic_blueprint
+from app.mod_scan.views import scan_blueprint
 
 # register the blueprints
 app.register_blueprint(anomalies_blueprint)
 app.register_blueprint(dynamic_blueprint)
+app.register_blueprint(scan_blueprint)
+
---
+++ app/mod_scan/views.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+"""
+A-Detector
+Licence: GPLv3
+Autor: @alexfrancow
+"""
+
+#################
+#### imports ####
+#################
+
+import os
+from flask import Flask, jsonify, Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+################
+#### config ####
+################
+
+scan_blueprint = Blueprint('scan', __name__, template_folder='templates')
+
+UPLOAD_FOLDER = 'app/mod_scan/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+###################
+#### functions ####
+###################
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def isolation_forest():
+    return 0
+
+################
+#### routes ####
+################
+
+@scan_blueprint.route('/scan', methods= ['GET', 'POST'])
+def scan():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            error = "No selected file"
+            return redirect("/scan", error=error)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/anomalies')
+    else:
+        return render_template("scan.html")
+
+if __name__ == "__main__":
+        app.run(debug=True)
